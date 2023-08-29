cyberknight777/dragonheart_kernel_oneplus_sm8150

treewide: Suppress logspam
- These logs are totally unnecessary on a production device.

Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -15479,7 +15479,7 @@ static ssize_t wlan_hdd_state_ctrl_param_write(struct file *filp,
 	}
 
 	if (strncmp(buf, wlan_on_str, strlen(wlan_on_str)) == 0)
-		pr_info("Wifi Turning On from UI\n");
+		pr_debug("Wifi Turning On from UI\n");
 
 	if (strncmp(buf, wlan_on_str, strlen(wlan_on_str)) != 0) {
 		pr_err("Invalid value received from framework");
@@ -15582,7 +15582,7 @@ static int  wlan_hdd_state_ctrl_param_create(void)
 		goto cdev_add_err;
 	}
 
-	pr_info("wlan_hdd_state %s major(%d) initialized",
+	pr_debug("wlan_hdd_state %s major(%d) initialized",
 		WLAN_MODULE_NAME, wlan_hdd_state_major);
 
 	return 0;
@@ -15604,7 +15604,7 @@ static void wlan_hdd_state_ctrl_param_destroy(void)
 	class_destroy(class);
 	unregister_chrdev_region(device, dev_num);
 
-	pr_info("Device node unregistered");
+	pr_debug("Device node unregistered");
 }
 
 /**
@@ -16418,7 +16418,7 @@ static void hdd_driver_unload(void)
 	QDF_STATUS status;
 	void *hif_ctx;
 
-	pr_info("%s: Unloading driver v%s\n", WLAN_MODULE_NAME,
+	pr_debug("%s: Unloading driver v%s\n", WLAN_MODULE_NAME,
 		QWLAN_VERSIONSTR);
 
 	/*

Pc1598/kernel_xiaomi_raphael

drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -15531,7 +15531,7 @@ static ssize_t wlan_hdd_state_ctrl_param_write(struct file *filp,
 	}
 
 	if (strncmp(buf, wlan_on_str, strlen(wlan_on_str)) == 0) {
-		hdd_info("Wifi Turning On from UI\n");
+		hdd_debug("Wifi Turning On from UI\n");
 		turning_on = true;
 	}
 
@@ -15638,7 +15638,7 @@ static int  wlan_hdd_state_ctrl_param_create(void)
 		goto cdev_add_err;
 	}
 
-	pr_info("wlan_hdd_state %s major(%d) initialized",
+	pr_debug("wlan_hdd_state %s major(%d) initialized",
 		WLAN_MODULE_NAME, wlan_hdd_state_major);
 
 	return 0;
@@ -15660,7 +15660,7 @@ static void wlan_hdd_state_ctrl_param_destroy(void)
 	class_destroy(class);
 	unregister_chrdev_region(device, dev_num);
 
-	pr_info("Device node unregistered");
+	pr_debug("Device node unregistered");
 }
 
 /**
@@ -16474,7 +16474,7 @@ static void hdd_driver_unload(void)
 	QDF_STATUS status;
 	void *hif_ctx;
 
-	pr_info("%s: Unloading driver v%s\n", WLAN_MODULE_NAME,
+	pr_debug("%s: Unloading driver v%s\n", WLAN_MODULE_NAME,
 		QWLAN_VERSIONSTR);
 
 	/*